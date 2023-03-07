9c72fa133ac1560616176f8568fbd4ba862178df
Gui Seek <guiseek@gmail.com>
2023-03-07 13:53:43 -0300

    feat: restart

--- src/domain/entities/player.ts
+++ src/domain/entities/player.ts
@@ -20,6 +20,105 @@ const PLAYER_FRAMES: PlayerFrames = [
   ['./player/hart-attack/6.png', 'hartAttack', 5],
 ]
 
+type FrameKey =
+  | 'waiting'
+  | 'landing'
+  | 'running'
+  | 'frontFlip'
+  | 'backFlip'
+  | 'tilt'
+  | 'superMan'
+  | 'hartAttack'
+
+export class Rider {
+  sprites = new Image()
+  targetMode: FrameKey = 'waiting'
+  frameIndex: Record<FrameKey, number[]> = {
+    waiting: [0],
+    landing: [1],
+    running: [2],
+    frontFlip: [3],
+    backFlip: [4],
+    tilt: [5],
+    superMan: [6, 7, 8, 9],
+    hartAttack: [10, 11, 12, 13, 14, 15],
+  }
+  mode: FrameKey = 'waiting'
+  frame = 0
+
+  constructor() {
+    this.sprites.src = 'frames.png'
+    const gameLoop = new GameLoop(this)
+    gameLoop.execute()
+  }
+
+  wait() {
+    this.targetMode = 'waiting'
+  }
+
+  land() {
+    this.targetMode = 'landing'
+  }
+
+  run() {
+    this.targetMode = 'running'
+  }
+
+  tilt() {
+    this.targetMode = 'tilt'
+  }
+
+  flip(dir: 'front' | 'back') {
+    this.targetMode = `${dir}Flip`
+  }
+
+  trick(mode: 'superMan' | 'hartAttack') {
+    this.targetMode = mode
+  }
+
+  tick() {
+    // this advances the frame and the robot
+    // the return value is how many pixels the robot has moved
+    this.frame += 1
+    if (this.frame >= this.frameIndex[this.mode].length) {
+      // we've reached the end of this animation cycle
+      this.frame = 0
+      if (this.mode != this.targetMode) {
+        // switch to next cycle
+        if (this.mode == 'running') {
+          // we need to stop walking before we decide what to do next
+          this.mode = 'waiting'
+        } else if (this.mode == 'waiting') {
+          if (this.targetMode == 'running') this.mode = 'running'
+          else this.mode = 'waiting'
+        } else if (this.mode == 'landing') {
+          if (this.targetMode == 'running') this.mode = 'running'
+        }
+      }
+    }
+    if (this.mode == 'tilt') return 8
+    return 0
+  }
+
+  paint(context: CanvasRenderingContext2D, x: number, y: number) {
+    if (!this.sprites.complete) return
+    // draw the right frame out of the sprite sheet onto the canvas
+    // we assume each frame is as high as the sprite sheet
+    // the x,y coordinates give the position of the bottom center of the sprite
+    context.drawImage(
+      this.sprites,
+      this.frameIndex[this.mode][this.frame] * this.sprites.height,
+      0,
+      this.sprites.height,
+      this.sprites.height,
+      x - this.sprites.height / 2,
+      y - this.sprites.height,
+      this.sprites.height,
+      this.sprites.height
+    )
+  }
+}
+
 export class Player {
   reloading = false
   position: Position = {
@@ -42,10 +141,14 @@ export class Player {
 
   constructor() {
     this.img.src = 'rider.svg'
-
+    onkeyup = ({key}) => {
+      if (key === 'r') {
+        this.restart()
+      }
+    }
     Promise.allSettled(loadFrames(frame, PLAYER_FRAMES)).then(() => {
-      const gameLoop = new GameLoop(this)
-      gameLoop.execute()
+      // const gameLoop = new GameLoop(this)
+      // gameLoop.execute()
     })
   }
 
@@ -124,6 +227,8 @@ export class Player {
 
     if (this.position.r > Math.PI) this.position.r = -Math.PI
     if (this.position.r < -Math.PI) this.position.r = Math.PI
+
+    // console.log(this.position.r);
 
     CONTEXT.save()
     CONTEXT.translate(this.position.x, this.position.y)
